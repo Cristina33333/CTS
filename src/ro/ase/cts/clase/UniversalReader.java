8451d1327dd3a81e8225a598514240f810900406
sauron <sauron@io>
2021-03-20 09:25:27 +0200

    test

--- src/ro/ase/cts/clase/UniversalReader.java
+++ src/ro/ase/cts/clase/UniversalReader.java
@@ -9,12 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import ro.ase.cts.clase.readers.Reader;
+
+
+//fatada pentru readere
 public class UniversalReader {
 
 	//punct de legatura cu readere
 	//todo
 	
-
+	public static List<Aplicant> readAplicants(String fileName, Reader reader){
+		
+		return reader.aplicantReader();
+	}
 	
 
 }
